d417f160e8069555e438b7aacfbbeb48a7a50108
soufunck <62726410+soufunck@users.noreply.github.com>
2023-06-25 23:40:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/* Animation */
+AOS.init({
+    disable: false,
+    delay: 0,
+    duration: 400,
+    easing: 'ease',
+    once: true,
+    mirror: false
+});
+
 /* Theme */
 const chk1 = document.getElementById('chk1');
 const chk2 = document.getElementById('chk2');
@@ -9,16 +19,3 @@ chk1.addEventListener('change', () => {
 chk2.addEventListener('change', () => {
   document.body.classList.toggle('tema-dark');  
 })
-
-// Scroll Reveal effect
-const sr = ScrollReveal({ distance: '1000px', duration: 500, reset: false });
-
-sr.reveal(
-    `.social,
-    .services .card .content, 
-    .works_card`, { 
-        origin: 'left', interval: 300 
-});
-
-sr.reveal(`.section-quem-somos .content p`, { origin: 'right', interval: 300 });
-sr.reveal(`.section-quem-somos .title`, { origin: 'top', interval: 300 });
